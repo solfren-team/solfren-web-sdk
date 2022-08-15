1613e8e80d7763f4729ecb1600ce633680ab226d
Jay Chung <ken8203@gmail.com>
2022-08-15 23:34:46 +0800

    refactor: rename options config

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,28 @@
 import { NFTFeed } from './modules/feed';
 import Collection from './modules/collection';
-import { Options } from './options';
+import { Config } from './types';
 
 export default class SolFrenSDK {
-  private options: Options;
+  private config: Config;
 
   // modules
   private nftFeed?: NFTFeed;
   private collection?: Collection;
 
-  public constructor(options: Options) {
-    this.options = options;
+  public constructor(config: Config) {
+    this.config = config;
   }
 
   public getNFTFeed(): NFTFeed {
     if (!this.nftFeed) {
-      this.nftFeed = new NFTFeed(this.options);
+      this.nftFeed = new NFTFeed(this.config);
     }
     return this.nftFeed;
   }
 
   public getCollection(): Collection {
     if (!this.collection) {
-      this.collection = new Collection(this.options);
+      this.collection = new Collection(this.config);
     }
     return this.collection;
   }
