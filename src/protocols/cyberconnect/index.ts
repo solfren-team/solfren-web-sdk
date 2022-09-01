7d771e728eac133c503f69594332c84a3e9621f0
LeoZhan <leo3838ok@gmail.com>
2022-09-01 22:50:29 +0800

    refactor(protocol): revise getIdentity to getFollowingCount

--- src/protocols/cyberconnect/index.ts
+++ src/protocols/cyberconnect/index.ts
@@ -1,5 +1,5 @@
 import { GraphQLClient, gql } from 'graphql-request'
-import { Identity } from './types';
+import { FollowingCount } from './types';
 
 export default class CyberConnect {
   private client: GraphQLClient;
@@ -8,52 +8,24 @@ export default class CyberConnect {
     this.client = new GraphQLClient(endpoint);
   }
 
-  public async getIdentity(walletAddress: string, limit: number = 30, cursor: string = "-1"): Promise<Identity | null> {
+  public async getFollowingCount(walletAddress: string): Promise<FollowingCount | null> {
     const query = gql`
-      query(
-        $address: String!
-        $first: Int
-        $after: String
-      ) {
+      query($address: String!) {
         identity(address: $address) {
           followerCount
           followingCount
-          followers(
-            first: $first
-            after: $after
-          ) {
-            pageInfo {
-              hasNextPage
-            }
-            list {
-              address
-            }
-          }
-          followings(
-            first: $first
-            after: $after
-          ) {
-            pageInfo {
-              hasNextPage
-            }
-            list {
-              address
-            }
-          }
         }
       }
     `;
     const variables = {
       address: walletAddress,
-      first: limit,
-      after: cursor,
     };
 
     return this.client
-      .request<{ identity: Identity }>(query, variables)
+      .request<{ identity: FollowingCount }>(query, variables)
       .then((res) => res.identity)
       .catch((err) => {
-        console.error('failed to getIdentity', err);
+        console.error('failed to getFollowingCount', err);
         return null;
       });
   }
--- src/protocols/cyberconnect/types.ts
+++ src/protocols/cyberconnect/types.ts
@@ -1,25 +1,4 @@
-export type Identity = {
+export type FollowingCount = {
   followerCount: number;
   followingCount: number;
-
-  followers: SocialConnectionsPaginated;
-  followings: SocialConnectionsPaginated;
-};
-
-export type SocialConnectionsPaginated = {
-  pageInfo: {
-    hasNextPage: boolean;
-    hasPreviousPage: boolean;
-    startCursor: string;
-    endCursor: string;
-  };
-  list: SocialConnection[];
-};
-
-export type SocialConnection = {
-  address: string;
-  alias: string;
-  avatar: string;
-  domain: string;
-  ens: string;
 };
