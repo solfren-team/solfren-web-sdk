d81d184b2c8db452864745e2ef9028508557227f
LeoZhan <leo3838ok@gmail.com>
2022-11-13 13:11:43 +0800

    feat(protocol): implement count followers and followings methods

--- src/protocols/solfren-follow/index.ts
+++ src/protocols/solfren-follow/index.ts
@@ -72,6 +72,24 @@ export default class SolFrenFollow {
     }
   }
 
+  public async countFollowers(walletAddress: string, type: string): Promise<number> {
+    if (type !== 'Wallet' && type !== 'NFT' && type !== 'Collection') {
+      throw new Error(`follow type not supported`);
+    }
+
+    const session = this.driver.session({ database: 'neo4j' });
+    try {
+      const writeQuery = `MATCH (:${type} { address: $walletAddress })<-[:FOLLOWS]-(w) RETURN count(w)`;
+      const writeResult = await session.writeTransaction(tx => tx.run(writeQuery, { walletAddress }));
+
+      return writeResult.records[0].get('count(w)').toNumber();
+    } catch (err) {
+      throw new Error(`failed to countFollowers: ${err}`);
+    } finally {
+      await session.close();
+    }
+  }
+
   public async listFollowings(walletAddress: string, type: string): Promise<Follow[]> {
     if (type !== 'Wallet') {
       throw new Error(`follow type not supported`);
@@ -88,7 +106,25 @@ export default class SolFrenFollow {
 
       return follows;
     } catch (err) {
-      throw new Error(`failed to listFollowing: ${err}`);
+      throw new Error(`failed to listFollowings: ${err}`);
+    } finally {
+      await session.close();
+    }
+  }
+
+  public async countFollowings(walletAddress: string, type: string): Promise<number> {
+    if (type !== 'Wallet') {
+      throw new Error(`follow type not supported`);
+    }
+
+    const session = this.driver.session({ database: 'neo4j' });
+    try {
+      const writeQuery = `MATCH (:Wallet { address: $walletAddress })-[:FOLLOWS]->(w:${type}) RETURN count(w)`;
+      const writeResult = await session.writeTransaction(tx => tx.run(writeQuery, { walletAddress }));
+
+      return writeResult.records[0].get('count(w)').toNumber();
+    } catch (err) {
+      throw new Error(`failed to countFollowings: ${err}`);
     } finally {
       await session.close();
     }
